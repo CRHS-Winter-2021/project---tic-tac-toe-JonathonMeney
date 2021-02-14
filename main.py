4a0d2b4da650d9616e82b16caa1a541561ae2e79
Jonathon Meney <jome3395@cloud.edu.pe.ca>
2021-02-14 20:51:39 +0000

    all working properly need to fix win as board full

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 ##Tic Tac Toe
 #Name: Jonathon Meney
-#Date: Feb. 11/21
+#Date: Feb. 12/21
 
-import sys 
 import time
-
+import os
 
 #1. (Var) Setup the empty board as a list
-theBoard = ['',
+theBoard = ['null',
             ' ',' ',' ',
             ' ',' ',' ',
             ' ',' ',' ']
@@ -24,8 +23,6 @@ def printBoard(board):
     print('-----')
     print(board[7] + '|' + board[8] + '|' + board[9])
 
-printBoard(theBoard)
-
 #3a. (fun) Determine if player is X or O
 player1 = ''
 player2 = ''
@@ -35,33 +32,46 @@ player2 = ''
 #out: None
 
 def chooseLetter():
-    
-    letterchoice = input('What letter would you like to play as?\n> ')
-    global player1
-    global player2
-
-    if letterchoice.upper() == 'X':
-      player1 = 'X'      
-      player2 = 'O'
-
-    elif letterchoice.upper() == 'O':
-      player1 = 'X'
-      player2 = 'O'
+	letterchoice = input('What letter would you like to play as?\n(X or O)\n> ')
+	global player1
+	global player2
+		
+	if letterchoice.upper() == 'X':
+		player1 = 'X'
+		player2 = 'O'
+		
+	elif letterchoice.upper() == 'O':
+		player1 = 'X'
+		player2 = 'O'
+	
+	else:
+		os.system('clear')
+		print('Invalid')
+		chooseLetter()
 
 #3b. (fun) Choose starting player 1 or 2
 playermove1 = ''
 playermove2 = ''
 
 def chooseStart():
-    movechoice = int(input('Player 1, would you like first or second move?\n(1 or 2)\n> '))
-    global playermove1
-    global playermove2
-    if movechoice == 1:
-      playermove1 = 'first'
-      playermove2 = 'second'
-    elif movechoice == 2:
-      playermove1 = 'second'
-      playermove2 = 'first'
+	try:
+		movechoice = int(input('Player 1, would you like first or second move?\n(1 or 2)\n> '))
+		global playermove1
+		global playermove2
+		if movechoice == 1:
+			playermove1 = 'first'
+			playermove2 = 'second'
+		elif movechoice == 2:
+			playermove1 = 'second'
+			playermove2 = 'first'
+		else:
+			os.system('clear')
+			print('Invalid')
+			chooseStart()
+	except:
+		os.system('clear')
+		print('Invalid')
+		chooseStart()
 
 #4. (fun) Get player move
 #in: board as list, player as X or O
@@ -71,43 +81,57 @@ def chooseStart():
 #out: none
 
 def playerMove(board, player):
-    global player1
-    global player2
-    playersmove = int(input('What is your move?\n> '))
-    if board[playersmove] == ' ':
-      if player == player1:
-        board[playersmove] = player1
-      elif player == player2:
-        board[playersmove] = player2
-    else:
-      return False
-
+	global player1
+	global player2
+	playersmove = int(input(player + ' what is your move?\n> '))
+	try:
+		if board[playersmove] == ' ':
+			if player == player1:
+				board[playersmove] = player1
+			elif player == player2:
+				board[playersmove] = player2
+		else:
+			print('That space is taken.')
+			playerMove(board,player)
+	except:
+		print('Invalid')
+		playerMove(board,player)
 
 #5. (fun) Check Winner
 #in: board as list, player as X or O
 #do: check all possible win scenarios
 #out: True for win, False otherwise
 
-
-
 def checkWin(board, player):
-    VicCons = [
-      #Horizontal wins [0,1,2]
+	toWin = 0
+
+	VicCons = [
+      #Horizontal wins
       [1,2,3],
       [4,5,6],
       [7,8,9],
-      #Vertical wins [3,4,5]
+      #Vertical wins
       [1,4,7],
       [2,5,8],
       [3,6,9],
-      #Diagonal wins [6,7]
+      #Diagonal wins
       [1,5,9],
       [3,5,7]
     ]
+	
+	for WinSet in VicCons:
+		for index in WinSet:
+			if board[index] == player:
+				toWin += 1
+			
+			else:
+				toWin = 0
 
-    for i in VicCons:
-      
-
+		if toWin == 3:
+			return True
+	
+	else:
+		return False        
 
 #6. (fun) Check if board is full
 #Because there are 10 list items for 9 spots,
@@ -117,29 +141,183 @@ def checkWin(board, player):
 #out: return True if board is full, False otherwise
 
 def checkFull(board):
-    pass
+    if board.count(' ') == 0:
+      return True
+    else:
+      return False
 
+
+def mainMenu():
+		global runGame
+		def titlescreen():
+			print ()
+			print ('         -Tic Tac Toe-          ')
+			print ()
+			print ('--------------------------------')
+			print ('             -Play-             ')
+			print ('             -Help-             ')
+			print ('             -Quit-             ')
+			print ('--------------------------------')
+			print ()
+			print ('Copyright © 2019 by Jonathon Meney')
+			print ()
+		
+		def showInstructions():
+			print('Use the numpad to choose your move.')
+  
+		titlescreen()
+		choice = input('What would you like to do?\n> ')
+		choice = choice.lower()
+		
+		#Menu selector
+		global runGame
+		if choice == 'play':
+			os.system('clear')
+			runGame = True
+		
+		elif choice == 'help':
+			os.system('clear')
+			showInstructions()
+			back = input ('\nType menu to go back\n> ')
+			if back == 'menu':
+				os.system('clear')
+				mainMenu()
+			
+			elif choice == 'quit':
+				os.system('clear')
+				print ('Leaving so soon? Are you sure you don\'t want to play my epic game?')
+				quitchoice = input('Yes or No?\n> ')
+			
+			if quitchoice == 'yes':
+				print ('Thanks for playing.')
+			
+			elif quitchoice == 'no':
+				os.system('clear')
+				print ('You came back! Thats great just choose your option and be on your way.')
+				mainMenu()
+		
+		else:
+			os.system('clear')
+			print ('My operating systems can\'t seem to understand that try again')
+			mainMenu()
+
+runGame = False
 #7. Main function
-
 def main():
-    #print Welcome
-    #print instructions
+	global runGame
+	global player1
+	global player2
+	global playermove1
+	global playermove2
 
-    #game play
-    #get player letter choice
-    
-    #while board is not full
-    ###first player move
-        #player chooses move
-        #print board
-        #check win
-        #check board full
+	#print Welcome
+	mainMenu()
+  #get player letter choice
+	chooseLetter()
+	os.system('clear')	
+	#get player to start choice
+	chooseStart()
+	os.system('clear')
 
-    ###second player move
-        #player chooses move
-        #print baord
-        #check win
-    
-    
-    pass
+	#game play
+	while runGame == True:
+		
+		if playermove1 == 'first':
+		#Player 1's Move
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#player chooses move
+			playerMove(theBoard,player1)
+			
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#check win
+			if checkWin(theBoard,player1) == True:
+				runGame = False
+				print(player1 + ' has won!')
+				break
 
+			#check board full
+			if checkFull(theBoard) == True:
+				runGame = False
+				print('The Board is full!\nGame Over!')
+				break
+			
+		#Player 2's Move
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#player chooses move
+			playerMove(theBoard,player2)
+			
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#check win
+			if checkWin(theBoard,player2) == True:
+				runGame = False
+				print(player2 + ' has won!')
+				break
+
+			#check board full
+			if checkFull(theBoard) == True:
+				runGame = False
+				print('The Board is full!\nGame Over!')
+				break
+
+		elif playermove2 == 'first':
+		#Player 2's Move
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#player chooses move
+			playerMove(theBoard,player2)
+			
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#check win
+			if checkWin(theBoard,player2) == True:
+				runGame = False
+				print(player2 + ' has won!')
+				break
+
+			#check board full
+			if checkFull(theBoard) == True:
+				runGame = False
+				print('The Board is full!\nGame Over!')
+				break
+			
+		#Player 1's Move
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#player chooses move
+			playerMove(theBoard,player1)
+			
+			#print board
+			os.system('clear')
+			printBoard(theBoard)
+			
+			#check win
+			if checkWin(theBoard,player1) == True:
+				runGame = False
+				print(player1 + ' has won!')
+				break
+
+			#check board full
+			if checkFull(theBoard) == True:
+				runGame = False
+				print('The Board is full!\nGame Over!')
+				break
+
+main()
